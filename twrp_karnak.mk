f45fc82404b6b2a44d0430a78e85b91745503a54
Kai Jones <kvjones0243@proton.me>
2022-12-28 22:33:11 -0800

    karnak: AndroidProducts: Change `omni_karnak` to `twrp_karnak`

--- twrp_karnak.mk
+++ twrp_karnak.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The Android Open-Source Project
+# Copyright 2022 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/amazon/karnak/device.mk)
 
-PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_karnak.mk
+PRODUCT_NAME := twrp_karnak
+PRODUCT_DEVICE := karnak
+PRODUCT_BRAND := Android
+PRODUCT_MANUFACTURER := Amazon
