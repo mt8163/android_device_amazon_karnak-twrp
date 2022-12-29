a83e98c38c07eb23c1a36239acf09acfaabeccd7
Kai Jones <kvjones0243@proton.me>
2022-12-28 22:42:26 -0800

    karnak:configs: inherit from common AOSP Config
    Signed-off-by: Kai Jones <kvjones0243@proton.me>

--- device.mk
+++ device.mk
@@ -1,1 +1,3 @@
-$(call inherit-product, build/target/product/full_base_telephony.mk)
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
